0f67b0ab7c392d9acb755d09be4200b204a70ca8
infern <semenmochalov55@gmail.com>
2023-09-15 19:58:38 +0700

    Работа с scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 import gulp from 'gulp';
-import fileInclude from 'gulp-file-include'
+import fileInclude from 'gulp-file-include';
+import gulpSass from 'gulp-sass';
+import * as sass from 'sass'
+
+const scss = gulpSass(sass);
 
 const fileIncludeSettings = {
     prefix: '@@',
@@ -10,4 +14,10 @@ gulp.task('includeFiles', function () {
     return gulp.src('./src/*.html')
         .pipe(fileInclude(fileIncludeSettings))
         .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('scss', function () {
+    return gulp.src('./src/scss/*.scss')
+        .pipe(scss())
+        .pipe(gulp.dest('./dist/css/'))
 });
