575734257d2e58b4359397a29daa73468c29f23b
infern <semenmochalov55@gmail.com>
2023-09-15 23:57:57 +0700

    Работа с js

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,9 @@ import sourceMaps from 'gulp-sourcemaps';
 // import groupMedia from 'gulp-group-css-media-queries';
 import plumber from 'gulp-plumber';
 import notify from 'gulp-notify';
+import webpack from 'webpack-stream';
+import webpackConfig from './webpack.config.js';
+import babel from 'gulp-babel';
 
 const scss = gulpSass(sass);
 
@@ -22,25 +25,20 @@ const serverSettings = {
     open: true,
 }
 
-const plumberSCSSSettings = {
-    errorHandler: notify.onError({
-        title: 'Styles',
-        message: 'Error <%= error.message %>',
-        sound: false,
-    })
+const plumberNotify = (title) => {
+    return {
+        errorHandler: notify.onError({
+            title: title,
+            message: 'Error <%= error.message %>',
+            sound: false,
+        }),
+    };
 }
 
-const plumberHTMLSettings = {
-    errorHandler: notify.onError({
-        title: 'HTML',
-        message: 'Error <%= error.message %>',
-        sound: false,
-    })
-}
 
 gulp.task('clean', function (done) {
     if (fs.existsSync('./dist/')) {
-        return gulp.src('./dist/', { read: false })
+        return gulp.src('./dist/', {read: false})
             .pipe(clean());
     }
     done();
@@ -48,20 +46,28 @@ gulp.task('clean', function (done) {
 
 gulp.task('html', function () {
     return gulp.src('./src/*.html')
-        .pipe(plumber(plumberHTMLSettings))
+        .pipe(plumber(plumberNotify('HTML')))
         .pipe(fileInclude(fileIncludeSettings))
         .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('scss', function () {
     return gulp.src('./src/scss/*.scss')
-        .pipe(plumber(plumberSCSSSettings))
+        .pipe(plumber(plumberNotify('SCSS')))
         .pipe(sourceMaps.init())
         .pipe(scss())
         // .pipe(groupMedia())
         .pipe(sourceMaps.write())
         .pipe(gulp.dest('./dist/css/'));
 });
+
+gulp.task('js', function () {
+    return gulp.src('./src/js/*.js')
+        .pipe(plumber(plumberNotify('JS')))
+        .pipe(babel())
+        .pipe(webpack(webpackConfig))
+        .pipe(gulp.dest('./dist/js'));
+})
 
 gulp.task('images', function () {
     return gulp.src('./src/img/**/*')
@@ -89,10 +95,11 @@ gulp.task('watch', function () {
     gulp.watch('./src/img/**/*', gulp.parallel('images'))
     gulp.watch('./src/fonts/**/*', gulp.parallel('fonts'))
     gulp.watch('./src/files/**/*', gulp.parallel('files'))
+    gulp.watch('./src/js/**/*.js', gulp.parallel('js'))
 })
 
 gulp.task('default', gulp.series(
     'clean',
-    gulp.parallel('html', 'scss', 'images', 'fonts', 'files'),
+    gulp.parallel('html', 'scss', 'images', 'fonts', 'files', 'js'),
     gulp.parallel('server', 'watch'),
 ))
