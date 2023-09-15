6bac05390c721bdc33f968d7617af05f5f06302c
infern <semenmochalov55@gmail.com>
2023-09-16 02:03:08 +0700

    Работа с пакетами npm

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,7 @@
 import hello from "./modules/hello.js";
+import AirDatepicker from "air-datepicker";
+import 'air-datepicker/air-datepicker.css';
+
+new AirDatepicker('#date');
 
 console.log(hello);
--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ const config = {
         rules: [
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader', 'babel-loader'],
+                use: ['style-loader', 'css-loader'],
             },
         ],
     },
