fe0a40aa43722ae0e8aaa105631f48471571ce08
infern <semenmochalov55@gmail.com>
2023-09-21 04:42:26 +0700

    Черновая верстка главной страницы (надо будет почистить код)

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,53 @@
 import mobileNav from './modules/mobile-nav.js';
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css/grid';
+
 mobileNav();
+
+const swiper = new Swiper('.service__slider', {
+    slidesPerView: 4,
+    spaceBetween: 23,
+    grid: {            //added
+        rows: 2,         //added
+    },                 //added
+    pagination: {
+        // el: '.swiper-pagination',
+    },
+    navigation: {
+        nextEl: ".swiper-next",
+        prevEl: ".swiper-prev",
+    },
+    updateOnWindowResize: true,
+    modules: [Navigation, Pagination],
+    scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: false,
+    },
+    breakpoints: {
+        1200: {
+            slidesPerView: 4,
+        },
+        1024: {
+            slidesPerView: 3,
+            grid: {            //added
+                rows: 2,         //added
+            },
+        },
+        768: {
+            slidesPerView: 2,
+            grid: {            //added
+                rows: 3,         //added
+            },
+        },
+        0: {
+            slidesPerView: 1,
+            grid: {
+                fill: 'row',
+                rows: 4,         //added
+            },
+        }
+
+    }
+})
